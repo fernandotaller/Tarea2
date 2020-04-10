b8a5620abaf72981097cfec816daeed7c4d6b84f
Fernando <frodriguezmonfort@gmail.com>
2020-04-09 23:59:49 -0300

    ClasesDelDominio - Punto A

---
+++ src/Usuario.java
@@ -0,0 +1,10 @@
+public class Usuario {
+
+    private String nombre;
+    private String cedula;
+    private String contrasena;
+    private int edad;
+    private int cantidadHijos;
+    private double saldo;
+
+}
