86740b093f7ffa4b40512afd637024b4b67e4d13
Fernando <frodriguezmonfort@gmail.com>
2020-04-10 00:18:13 -0300

    ClasesDelDominio - Punto B

--- src/Usuario.java
+++ src/Usuario.java
@@ -7,4 +7,62 @@ public class Usuario {
     private int cantidadHijos;
     private double saldo;
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getCantidadHijos() {
+        return cantidadHijos;
+    }
+
+    public void setCantidadHijos(int cantidadHijos) {
+        this.cantidadHijos = cantidadHijos;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public Usuario(String nombre, String cedula, String contrasena, int edad, int cantidadHijos) {
+        super();
+        this.nombre = nombre;
+        this.cedula = cedula;
+        this.contrasena = contrasena;
+        this.edad = edad;
+        this.cantidadHijos = cantidadHijos;
+        this.saldo = 0;
+    }
+
 }
